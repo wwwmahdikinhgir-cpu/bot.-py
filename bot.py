f9062f27597c4f7998d825a7c02df594c0fa145c
wwwmahdikinhgir-cpu <www.mahdikinhg.ir@gmail.com>
2026-08-27 15:29:50 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import os
 # ⚙️ تنظیمات - این قسمت را تغییر دهید
 # ============================================
 
-BOT_TOKEN = os.environ.get('8834771206:AAHXE7wBgFxKv2hy5FrGiJvJ7PV83c5Gsr0', 'YOUR_BOT_TOKEN_HERE')
+BOT_TOKEN =″8834771206:AAHXE7wBgFxKv2hy5FrGiJvJ7PV83c5Gsr0″
 ADMIN_ID = int(os.environ.get('mamad914', '7903950474'))
 CHANNEL_USERNAME = os.environ.get('CHANNEL_USERNAME', 'your_channel')
 CHANNEL_URL = os.environ.get('CHANNEL_URL', 'https://t.me/your_channel')
